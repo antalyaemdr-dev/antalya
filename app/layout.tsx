5a105aa2f1256e28a65f692e15f4a4538601ba0c
Ela Teknoloji <hytasarim@gmail.com>
2026-08-01 12:16:13 +0300

    search

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import CookieConsent from "@/components/CookieConsent";
 import { GoogleAnalytics } from '@next/third-parties/google';
 
+
 const nunito = Nunito({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
@@ -22,6 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
+      <head>
+        {/* GOOGLE SEARCH CONSOLE DOĞRULAMA KODU */}
+        <meta name="google-site-verification" content="Q1G906VmyOzEIna-_ZMgv0FYm9ahBvFbzNL5173O4ig" />
+      </head>
       <body className={`${nunito.className} min-h-screen flex flex-col antialiased bg-sand-light`}>
         {children}
         <CookieConsent /> {/* Çerez uyarısı artık body içinde ve görünür durumda */}
